db6d167d453617ef035074519f3045d379b9524a
3ryh_c00l <3rikpas@gmail.com>
2023-09-28 20:18:05 +0700

    counter on typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState<number>(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Typescript ~ Javascript</h1>
+
+      {counter}
+      <br />
+      <button onClick={()=>{
+        setCounter(counter+1)
+      }}
+      >
+        Increase 1
+      </button>
     </div>
   );
 }
